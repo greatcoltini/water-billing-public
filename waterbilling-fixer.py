f6807bb64a7dfa34afe66e75e9be6c894d7fd71a
greatcoltini <coltonmdonk@gmail.com>
2022-07-04 20:11:42 -0400

    Adjusted Name Scheme

--- waterbilling-fixer.py
+++ waterbilling-fixer.py
@@ -1,59 +1,95 @@
-# open all files for editing
-new_route = open("route_tests\WaterRoute220629.exp", "r")
-old_route = open("route_tests\WaterRoute220531.exp", "r")
-edited_file = open("edited_route_full.exp", "w")
-error_output = open("error_output.txt", "w")
+import os
 
-# we want to parse through the new route, and convert it to the old formatting style
-for line in new_route:
-
-    # initialization of variables to write
-    route_name = line[0:20]
-    id_number = line[28:38]
-    m_code_l = line[26:41]
-    m_code_no_l = line[28:40]
-    error_parsing_values = line[53:110]
-    measured_flow = line[69:78]
-    date_scheme = "062522062622"
-    account_specific_info = line[122:251]
-    remainder_of_line = line[267:]
-    has_acct_id = False
-
-    # parse through route of old format to find information not contained in new
-    for section in old_route:
-        # grab account id from old routing format
-        if id_number in section:
-            acct_id = section[250:].split(' ')[0]
-            has_acct_id = True
-
-    ## ERROR OUTPUTTING ##
-    # if there is no account id, likely a newly added home as of this month, so add to error output
-    if not has_acct_id:
-        error_output.write("ID Number: " + id_number + " did not have a previous account.\n")
-        acct_id = "ERROR"
-    # error output for flow read issue
-    if not measured_flow.isnumeric():
-        error_output.write("ID Number: " + id_number + " had a flow reading error.\n")
-    # error output for no flow this month
-    if measured_flow == "000000000":
-        error_output.write("ID Number: " + id_number + " had no flow this month.\n")
+# global errors list
+errors = []
 
 
-    # printing for debugging purposes
-    print(acct_id)
-    print(id_number)
-    print(measured_flow)
+# PARSES THE OLD ROUTE, ADDING THE NEW FLOW CALCULATED FROM THIS MONTH'S METER READS
+# DOES NOT ACCOUNT FOR NEW METERS ADDED HERE... MUST MANUALLY ADJUST.
+def parse_route(last_month, this_month):
+    print("Parsing old route and adding new route flow data...")
+    # open all files for editing
+    old_route = open(last_month, "r")
+    edited_file = open("edited_route_full_from_old.exp", "w")
+    for line in old_route:
 
-    # write out the new file
-    edited_file.write(route_name + "ES1000" + m_code_l + m_code_no_l + error_parsing_values
-                      + date_scheme + account_specific_info + acct_id + remainder_of_line)
+        # initialization of variables to write
+        route_number = line[0:20]
+        up_to_meter = line[20:41]
+        meter_id = line[28:38]
+        spacing_to_flow = line[52:68]
+        measured_flow = line[69:88]
+        spacing_to_date = line[89:108]
+        date = "062522062622"
+        spacing_to_routing = line[123:130]
+        routing_info = line[131:147]
+        remainder = line[148:]
+
+        # parse through new route, find id, add meter info
+        new_route = open(this_month, "r")
+        for section in new_route:
+            # grab account id from old routing format
+            compare_id = section[28:38]
+            if meter_id == compare_id:
+                measured_flow = section[68:78]
+                routing_info = section[131:147]
+                route_number = section[0:20]
+                break
+        new_route.close()
+
+        # write out the new file
+        edited_file.write(route_number + up_to_meter + meter_id + spacing_to_flow + measured_flow + spacing_to_date +
+                          date + spacing_to_routing + routing_info + remainder)
+
+    # close the files
+    old_route.close()
+    edited_file.close()
 
 
-# close all files once done editing
-new_route.close()
-edited_file.close()
-error_output.close()
-old_route.close()
+# CALCULATES WHICH METERS MAY BE IDENTIFIABLE AS NEW METERS
+# COMPARES NEW ROUTE IDS WITH OLD ROUTE, IF MISMATCH, MAY BE NEW METER
+def calculate_new_meters(last_month_imp, this_month_imp):
+    print("Determining which meters are new additions...")
+    # loop through each line of the new route, finding id
+    new_route_compare = open(this_month_imp, "r")
+
+    # we loop through the new route meters, and check if mismatch from old route, if so we know it's a new install
+    for section in new_route_compare:
+        meter_id = section[28:38]
+        new_meter = True
+        old_route_compare = open(last_month_imp, "r")
+        for line in old_route_compare:
+            compare_id = line[28:38]
+            if meter_id == compare_id:
+                new_meter = False
+                break
+        if new_meter:
+            error = "Meter: " + str(meter_id) + " is a newly added meter.\n"
+            errors.append(error)
 
 
+# WRITES ERRORS TO OUTPUT FILE FROM GLOBAL ERRORS LIST
+def write_errors():
+    print("Writing errors to file error_output.txt...\n")
+    print("Total of " + str(len(errors)) + " errors.\n")
+    error_output = open("error_output.txt", "w")
+    for string in errors:
+        error_output.write(string)
+    error_output.close()
 
+
+def main():
+    # run our functions
+    # take user input, to send to parse_route
+    last_month_imp = input("Enter the path of last month's route information: ")
+    this_month_imp = input("Enter the path of this month's route information: ")
+    assert os.path.exists(last_month_imp), "Error: no file at presented path " + str(last_month_imp)
+    assert os.path.exists(this_month_imp), "Error: no file at presented path " + str(last_month_imp)
+
+    parse_route(last_month_imp, this_month_imp)
+    calculate_new_meters(last_month_imp, this_month_imp)
+    write_errors()
+
+
+if __name__ == "__main__":
+    main()
